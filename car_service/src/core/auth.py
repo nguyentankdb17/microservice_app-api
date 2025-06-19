ec9c4d3ef3121524c321efc5ca0540f68412b80d
nguyentankdb17 <nguyentankdb17@gmail.com>
2025-06-19 11:35:10 +0700

    update authorization for api

--- car_service/src/core/auth.py
+++ car_service/src/core/auth.py
@@ -1,15 +1,32 @@
 import httpx
+from fastapi import HTTPException, Request, status
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-async def verify_user_token(token: str):
-    async with httpx.AsyncClient() as client:
-        headers = {"Authorization": f"Bearer {token}"}
-        try:
-            response = await client.get(f"{os.getenv("USER_SERVICE_URL")}/api/user/user-info", headers=headers)
-            response.raise_for_status()
-            return response.json() # return user information
-        except httpx.HTTPStatusError as e:
-            raise Exception("Unauthorized: Invalid token")
+async def verify_token(token: str):
+    headers = {"Authorization": f"Bearer {token}"}
+    try:
+        async with httpx.AsyncClient() as client:
+            res = await client.get(f"{os.getenv('USER_SERVICE_URL')}/api/user/user-info", headers=headers)
+            res.raise_for_status()
+            return res.json()
+    except httpx.HTTPStatusError as e:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid or expired token"
+        )
+    
+async def require_admin(request: Request):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise HTTPException(status_code=403, detail="Authorization header missing")
+
+    token = auth_header.replace("Bearer ", "")
+    user = await verify_token(token)
+
+    if not user.get("is_admin"):
+        raise HTTPException(status_code=403, detail="Admin feature only, you do not have permission to access this feature")
+
+    return user
--- car_service/src/routes/car.py
+++ car_service/src/routes/car.py
@@ -1,11 +1,12 @@
 from typing import List
-from fastapi import HTTPException, Depends
+from fastapi import HTTPException, Depends, Request
 from sqlalchemy.orm import Session
 from starlette import status
 import src.models as models
 import src.schemas as schemas
 from fastapi import APIRouter
 from src.database import get_db
+from src.core.auth import verify_token, require_admin
 
 # Initialize the router
 router = APIRouter(
@@ -13,15 +14,21 @@ router = APIRouter(
     tags=["cars"]
 )
 
-# Define the routes for getting the cars list
 @router.get("/list", response_model=List[schemas.Car])
-def get_cars(db: Session = Depends(get_db)):
+async def get_cars(request: Request, db: Session = Depends(get_db)):
+    auth_header = request.headers.get("Authorization")
+    if not auth_header or not auth_header.startswith("Bearer "):
+        raise HTTPException(status_code=403, detail="Authorization header missing")
+
+    token = auth_header.replace("Bearer ", "")
+    user = await verify_token(token)
+
     cars = db.query(models.Car).all()
     return cars
 
 # Define the routes for creating a car
 @router.post("/create", response_model=schemas.Car)
-def create_car(car: schemas.CreateCar, db: Session = Depends(get_db)):
+async def create_car(car: schemas.CreateCar, db: Session = Depends(get_db), user: dict = Depends(require_admin)):
     new_car = models.Car(**car.model_dump())
     db.add(new_car)
     db.commit()
@@ -30,7 +37,7 @@ def create_car(car: schemas.CreateCar, db: Session = Depends(get_db)):
 
 # Define the routes for updating a car
 @router.put("/update/{car_id}", response_model=schemas.Car)
-def update_car(car_id: int, car: schemas.Car, db: Session = Depends(get_db)):
+async def update_car(car_id: int, car: schemas.Car, db: Session = Depends(get_db), user: dict = Depends(require_admin)):
     db_car = db.query(models.Car).filter(models.Car.id == car_id).first()
     if not db_car:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="car not found")
@@ -40,9 +47,10 @@ def update_car(car_id: int, car: schemas.Car, db: Session = Depends(get_db)):
     db.refresh(db_car)
     return db_car
 
+
 # Define the routes for deleting a car
 @router.delete("/delete/{car_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_car(car_id: int, db: Session = Depends(get_db)):
+async def delete_car(request: Request, car_id: int, db: Session = Depends(get_db), user: dict = Depends(require_admin)):
     db_car = db.query(models.Car).filter(models.Car.id == car_id).first()
     if not db_car:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="car not found")
