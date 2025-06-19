2562ff6619f1539eb27a1ab9110a97e5f10bb236
nguyentankdb17 <nguyentankdb17@gmail.com>
2025-06-19 15:36:18 +0700

    update redis connection

--- user_service/src/utils/redis_client.py
+++ user_service/src/utils/redis_client.py
@@ -1,7 +1,11 @@
 import redis
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 redis_client = redis.Redis(
-    host="localhost",
+    host=os.getenv("REDIS_URL"),
     port=6379,
     db=0,
     decode_responses=True
