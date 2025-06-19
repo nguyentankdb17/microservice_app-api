01287fc5f0e62fe12290f1ecf5d93b55b2fb6eaa
nguyentankdb17 <nguyentankdb17@gmail.com>
2025-06-19 11:39:06 +0700

    increase token time

--- user_service/src/core/auth.py
+++ user_service/src/core/auth.py
@@ -27,7 +27,7 @@ def hash_password(password):
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=30))
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
