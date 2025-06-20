87c2db4e1239723bbf9f47cfd2b2b1a276617c99
nguyentankdb17 <nguyentankdb17@gmail.com>
2025-06-20 22:27:10 +0700

    add metrics exporter path

--- car_service/src/main.py
+++ car_service/src/main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
 from src.routes import car
-from src.core.middleware import add_middlewares
+from src.middleware.cors import add_cors
+from src.middleware.metrics import metrics_app, update_request_counter, update_system_metrics
 
 app = FastAPI()
-add_middlewares(app)
+add_cors(app)
+
+app.mount("/metrics", metrics_app)
+app.middleware("http")(update_request_counter)
+app.middleware("http")(update_system_metrics)
+
 app.include_router(car.router)
--- car_service/src/routes/car.py
+++ car_service/src/routes/car.py
@@ -6,11 +6,10 @@ import src.models as models
 import src.schemas as schemas
 from fastapi import APIRouter
 from src.database import get_db
-from src.core.auth import verify_token, require_admin
+from src.middleware.auth import verify_token, require_admin
 
 # Initialize the router
 router = APIRouter(prefix="/api/cars", tags=["cars"])
-
 
 @router.get("/list", response_model=List[schemas.Car])
 async def get_cars(request: Request, db: Session = Depends(get_db)):
@@ -34,6 +33,7 @@ async def create_car(
     db: Session = Depends(get_db),
     user: dict = Depends(require_admin),
 ):
+
     new_car = models.Car(**car.model_dump())
     db.add(new_car)
     db.commit()
@@ -49,6 +49,7 @@ async def update_car(
     db: Session = Depends(get_db),
     user: dict = Depends(require_admin),
 ):
+
     db_car = db.query(models.Car).filter(models.Car.id == car_id).first()
     if not db_car:
         raise HTTPException(
@@ -64,11 +65,11 @@ async def update_car(
 # Define the routes for deleting a car
 @router.delete("/delete/{car_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_car(
-    request: Request,
     car_id: int,
     db: Session = Depends(get_db),
     user: dict = Depends(require_admin),
 ):
+
     db_car = db.query(models.Car).filter(models.Car.id == car_id).first()
     if not db_car:
         raise HTTPException(
